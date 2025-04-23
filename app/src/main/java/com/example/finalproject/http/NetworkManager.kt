116e2e6c806f3cba9f0c68024051826d083d6040
NOA2901 <noa@links-workplace.com>
2025-04-23 18:30:46 +0300

    SearchBookAPI

--- app/src/main/java/com/example/finalproject/http/NetworkManager.kt
+++ app/src/main/java/com/example/finalproject/http/NetworkManager.kt
@@ -3,6 +3,9 @@ package com.example.finalproject.http
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object NetworkManager {
+
+
+
     const val API_KEY = "AIzaSyAaDjbF4uLjEMK53gVaeOoY0isxyrRmBAw"
 //    const val API_KEY = "AIzaSyAcJst4JfkSw6FfcljZ5gQt00pToYPt7q0"
     private const val BASE_URL = "https://www.googleapis.com/books/v1/"
